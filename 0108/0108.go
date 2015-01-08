3e17756942a365a5812e241f5e9d433752fc94ee
Kwag De Gi <code@skyaperture.com>
2015-01-08 10:54:23 +0900

    export convention

--- 0108/0108.go
+++ 0108/0108.go
@@ -4,12 +4,15 @@ import (
 	"fmt"
 	"time"
 	"math/rand"
+	"math"
 )
 
 func main() {
 	fmt.Println("Hello, 世界")
 	fmt.Println("The time is ", time.Now())
 	fmt.Println("My favorite number is", rand.Intn(10))
+	fmt.Printf("Now you have %g problems.", math.Nextafter(2,3))
+	fmt.Println(math.Pi)
 }
 
 
