981a20665aaa066141d9a2c139cd041ca787d23c
Kwag De Gi <code@skyaperture.com>
2015-02-17 08:19:51 +0900

    append to slice

---
+++ 0217/append.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a []int
+	printSlice("a", a)
+
+	a = append(a, 0)
+	printSlice("a", a)
+	
+	a = append(a, 1)
+	printSlice("a", a)
+
+	a = append(a, 2, 3, 4)
+	printSlice("a", a)
+}
+
+func printSlice(s string, x []int) {
+	fmt.Printf("%s len=%d cap=%d %v\n", s, len(x), cap(x), x)
+}
+
